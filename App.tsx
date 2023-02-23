05192edec571011a506d5dc56bc6882ecd976ca2
9utty <leegh4250@gmail.com>
2023-02-23 22:16:34 +0900

    refactor: App.tsx 수정

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import mobileAds, {
+  BannerAd,
+  BannerAdSize,
+} from "react-native-google-mobile-ads";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
+
+GoogleSignin.configure();
+
+mobileAds()
+  .initialize()
+  .then((result) => {
+    console.log(result);
+  });
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <RootApp />
+        <BannerAd unitId={TestIds.BANNER} size={BannerAdSize.FULL_BANNER} />
+      </Provider>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
